50149381c6db7467db716cb9fa7647b6b225da6b
gitXite <daniel.halaas@gmail.com>
2024-09-02 22:42:50 +0200

    added get_num_chars() function

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"The book contains {num_words} words!")
+    num_chars = get_num_chars(text)
+    print(num_chars)
+
+
+def get_num_chars(text):
+    char_dict = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        char_dict[char] = char_dict.get(char, 0) + 1
+    return char_dict
+
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
 
 def get_book_text(path):
     with open(path) as f:
