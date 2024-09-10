4e2aa9c1443416a7af67e8ae5e94609c6bf2b515
gitXite <daniel.halaas@gmail.com>
2024-09-10 22:53:25 +0200

    Added the rest of the print statements to complete the report. Missing sort_on function and the sorted characters.

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
-
+    
 
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
+    for key, value in num_chars.items():
+        print(f"The '{key}' character was found {value} times")    
+    print("--- End report ---")
+
+
+def sort_on(num_chars):
+    pass
 
 
 def get_num_chars(text):
     char_dict = {}
     lowered_text = text.lower()
+
     for char in lowered_text:
-        char_dict[char] = char_dict.get(char, 0) + 1
+        if char.isalpha():
+            char_dict[char] = char_dict.get(char, 0) + 1
     return char_dict
 
 
